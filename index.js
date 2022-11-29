058bc90222e5c9eebd918ea08d63f0358a0d1f7c
ravindra bagale <ravi2.bagale@gmail.com>
2022-11-29 06:13:11 +0000

    deploy added

--- index.js
+++ index.js
@@ -12,12 +12,12 @@
  
 // module.exports = app;
 const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = '0.0.0.0';
+const port = 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello World , have u changed?\n');
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
---
+++ script/deploy.sh
@@ -0,0 +1,3 @@
+ssh -i windows.pem ec2-user@13.234.112.190
+cd ~/node-app/
+git pull https://github.com/ravindrabagale/node-app.git
