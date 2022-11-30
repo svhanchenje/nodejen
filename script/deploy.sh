dbfd56f18bc02b60eef44c4639954ef9986e6e34
ravindrabagale <ravindra.bagale@hotmail.com>
2022-11-30 10:47:17 +0530

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,4 +1,4 @@
-ssh -tt -i windows.pem  ec2-user@43.205.239.76 << ENDSSH
+ssh -i windows.pem  ec2-user@43.205.239.76 << ENDSSH
 cd ~/node-app/
 git pull https://github.com/ravindrabagale/node-app.git
 ENDSSH
